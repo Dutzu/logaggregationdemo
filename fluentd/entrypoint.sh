19b698146c0b26d97f335b11705e5a55c3660aec
mihaido <doru.mihai@haufe-lexware.com>
2017-06-30 19:37:47 +0300

    Added cert and key injection to fluentd container

--- fluentd/entrypoint.sh
+++ fluentd/entrypoint.sh
@@ -5,6 +5,22 @@ CYAN='\033[0;36m'
 RED='\033[0;31m'
 NC='\033[0m' #No Color
 
+if [ ${#FLUENTD_CERT} -lt 2 ] ; then
+  echo -e "${RED}Certificate not found!${NC} Must load a certificate in pem form into ${GREEN}FLUENTD_CERT${NC}";
+else
+  echo "Found certificate passed as env var.";
+  echo ${FLUENTD_CERT} > /fluentd/certs/ca_cert.pem
+  sed -i 's/\\n/\n/g' /fluentd/certs/ca_cert.pem
+fi
+
+if [  ${#FLUENTD_CERT_KEY} -lt 2 ] ; then
+  echo -e "${RED}Certificate not found!${NC} Must load a certificate private key in pem form into ${GREEN}FLUENTD_CERT_KEY${NC}";
+else
+  echo "Found certificate passed as env var.";
+  echo ${FLUENTD_CERT_KEY} > /fluentd/certs/ca_key.pem
+  sed -i 's/\\n/\n/g' /fluentd/certs/ca_key.pem
+fi
+
 if [  ${FLUENTD_CONF} -lt 2 ]; then
   echo -e "${RED}Fluentd configuration not found!${NC} Must specify what configuration to load via ${GREEN}FLUENTD_CONF${NC}";
   exit 1;
